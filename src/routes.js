ca62d289e01e88dbddfeae0a522fd1dd34f4585a
matheusv11 <mathsales360@gmail.com>
2022-10-23 00:35:30 -0300

    Página Inicial

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const ticketController = require('./controllers/ticketController');
 
 // Routes
+router.get("/", (req, res) => res.send("<h2> Página Inicial </h2>"))
+
 router.post("/agendar-ticket", ticketController.registerTicket)
 
 module.exports = router;
