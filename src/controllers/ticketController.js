5fca7d2275aaa9c183ed44eb89e8b053d84aea7c
matheusv11 <mathsales360@gmail.com>
2022-10-24 11:23:37 -0300

    Não esperar pelo puppeteer

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -11,7 +11,7 @@ module.exports = {
       executablePath: await chrome.executablePath,
       headless: chrome.headless
     }).then(async browser => {
-      const tipo = 'jantar' // Temporário
+      const tipo = 'Jantar' // Temporário
 
       for await(let user of users) { // Com esse for, ele só vai um por vez // Com Promise.all() Abre tudo de uma vez
         const page = await browser.newPage();
@@ -94,85 +94,79 @@ module.exports = {
     const { login, senha, tipo } = req.body;
 
     // PUPPETEER CONFIG
-    const browser = await chrome.puppeteer.launch({
+    chrome.puppeteer.launch({
       args: chrome.args,
       executablePath: await chrome.executablePath,
       headless: chrome.headless
+    }).then(async browser => {
+      try {
+
+        const page = await browser.newPage();
+  
+        await page.setUserAgent(
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+        );
+  
+        // IR PARA O SARI
+        await page.goto("http://www.floriano.ifpi.edu.br:8080/CortexMobileIFPI/login.jsf");
+  
+        // REALIZAR LOGIN
+        await page.type("#j_username", login);
+        await page.type("#j_password", senha);
+        await page.click("input[name='j_idt6:j_idt26']");
+  
+        // VERIFICAR SE HÁ ERRO
+        const urlError= await page.url().includes("?erro=true");
+    
+        if(urlError){
+            await browser.close()
+            throw Error("Login inválido");
+        }
+  
+        //PÁGINA DE TICKETS
+        await page.goto("http://www.floriano.ifpi.edu.br:8080/CortexMobileIFPI/modulos/minhaConta/solicitarTickets.jsf");
+        
+        // VALIDAR DISPONIBILIDADE DE TICKET
+        const ticketId = await page.evaluate(val =>{
+          const [...divs] = document.querySelectorAll("div[class='ui-panel-m-titlebar ui-bar ui-bar-inherit']");
+
+          return divs.find(e => {
+            const [dia, tipo] = e.innerText.split(' - ');
+            return dia.includes(new Date().toLocaleDateString('pt-BR')) && tipo === val.tipo
+          })?.parentElement.id
+    
+        }, { tipo: tipo });
+
+        if(!ticketId) {
+          throw Error("O sistema do sari está fechado ao momento");
+        }
+  
+        const qtdTicket = await page.$eval(`div[id="${ticketId}"] div[class="ui-panel-m-content ui-body ui-body-inherit"] p`, e => e.innerText)
+        const totalTicket = qtdTicket.match('\W*(Qtd. Disponível: [0-9]+)\W*')[0].split(": ")[1]
+    
+        if(parseInt(totalTicket) <= 0) {
+          throw Error("Não há mais ticket disponíveis neste horário");
+        }
+    
+        // VALIDAR CAPTCHA
+        const divCaptcha = await page.waitForSelector('div#Captcha iframe');
+        const frame = await divCaptcha.contentFrame();
+        frame.$eval('span[id="recaptcha-anchor"]', e => e.click());
+    
+        // RESGATAR TICKET
+        await page.waitForTimeout(3000);
+    
+        await page.$eval(`button[id="${ticketId.replace('23', '40')}"]`, e => e.click());
+    
+        await browser.close();
+
+      } catch (e) {
+        console.error("ERROR=>", e);
+        await browser.close();
+      }
     });
 
-    try {
-
-      const page = await browser.newPage();
-
-      await page.setUserAgent(
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
-      );
-
-      // IR PARA O SARI
-      await page.goto("http://www.floriano.ifpi.edu.br:8080/CortexMobileIFPI/login.jsf");
-
-      // REALIZAR LOGIN
-      await page.type("#j_username", login);
-      await page.type("#j_password", senha);
-      await page.click("input[name='j_idt6:j_idt26']");
-
-      console.log("Loguei no SARI")
-
-      // VERIFICAR SE HÁ ERRO
-      const urlError= await page.url().includes("?erro=true");
-  
-      if(urlError){
-          await browser.close()
-          throw Error("Login inválido");
-      }
-
-      //PÁGINA DE TICKETS
-      await page.goto("http://www.floriano.ifpi.edu.br:8080/CortexMobileIFPI/modulos/minhaConta/solicitarTickets.jsf");
-      
-      // VALIDAR DISPONIBILIDADE DE TICKET
-      const ticketId = await page.evaluate(val =>{
-        const [...divs] = document.querySelectorAll("div[class='ui-panel-m-titlebar ui-bar ui-bar-inherit']")
-  
-        return divs.find(e => {
-          const [dia, tipo] = e.innerText.split(' - ');
-          return dia.includes(new Date().toLocaleDateString('pt-BR')) && tipo === val.tipo
-        })?.parentElement.id
-  
-      }, { tipo: tipo });
-      
-      if(!ticketId) {
-        throw Error("O sistema do sari está fechado ao momento");
-      }
-
-      const qtdTicket = await page.$eval(`div[id="${ticketId}"] div[class="ui-panel-m-content ui-body ui-body-inherit"] p`, e => e.innerText)
-      const totalTicket = qtdTicket.match('\W*(Qtd. Disponível: [0-9]+)\W*')[0].split(": ")[1]
-  
-      if(parseInt(totalTicket) <= 0) {
-        throw Error("Não há mais ticket disponíveis neste horário");
-      }
-  
-      // VALIDAR CAPTCHA
-      const divCaptcha = await page.waitForSelector('div#Captcha iframe');
-      const frame = await divCaptcha.contentFrame();
-      frame.$eval('span[id="recaptcha-anchor"]', e => e.click());
-  
-      // RESGATAR TICKET
-      await page.waitForTimeout(3000);
-  
-      await page.$eval(`button[id="${ticketId.replace('23', '40')}"]`, e => e.click());
-  
-      await browser.close();
-  
-      return res.status(200).send("Ticket Resgatado");
-  
-    } catch (e) {
-      console.error("ERROR=>", e);
-      await browser.close();
-
-      return res.status(500).send(`Ocorreu um problema: ${e}`);
-    }
-
-
+    return res.status(200).send("Ticket Resgatado");
   }
 
 }
